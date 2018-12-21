cd9085982babcfce0e6bac744117a595eb7f7c91
Dennis Tsiang <dt1015@ic.ac.uk>
2018-12-21 12:19:59 +0000

    Update energy-eval endpoint to take in filename and script
    The filename points to the APK file to evaluate. This should allow
    it to handle multiple concurrnt requests as well. multer has been
    changed so that the extension of the file is appended to the end of
    the filename too. The call to Orka now passes the filename and when
    Orka has finished it will delete the apk file. Will need to add
    the script to the parameter as well when the frontend has implemented
    ability to upload monkeyrunner scripts.

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    let ext = ''; // set default extension (if any)
+    // checking if there is an extension or not
+    if (file.originalname.split(".").length > 1) {
+      ext = file.originalname.substring(file.originalname.lastIndexOf('.'),
+               file.originalname.length);
+    }
+    cb(null, Date.now() + ext);
+  }
+});
+var upload = multer({ storage: storage })
+var fs = require('fs');
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
@@ -16,19 +31,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/file-upload', upload.single('file'), function (req, res) {
   console.log("Received a file upload POST request");
   console.log(req.file);
-  res.send('Received file successfully');
+  res.send({
+    'message': 'Recieved file successfully',
+    'filename': req.file.filename
+  });
 })
 
-app.get('/energy-eval', function(req, res) {
+app.get('/energy-eval/:filename/:script', function(req, res) {
+  if (req.params.filename == null) {
+    res.status(400);
+    res.send("There was an error parsing the filename parameter");
+    return;
+  }
+  if (req.params.script == null) {
+    res.status(400);
+    res.send("There was an error parsing the script parameter");
+    return;
+  }
   const spawn = require("child_process").spawn;
-  const orkaProcess = spawn('python',["vendor/orka/src/main.py"]);
+  const orkaProcess = spawn('python',["vendor/orka/src/main.py", "--app", 
+    "uploads/"+req.params.filename]);
   orkaProcess.stdout.setEncoding('utf-8');
   orkaProcess.stdout.on('data', function(data) {
     console.log(data);
   });
   orkaProcess.on('close', function(exitCode) {
     console.log("Orka process has finished");
-    res.send("Energy evaluation finished");
+    console.log(`Deleting apk file ${req.params.filename} ...`);
+    fs.unlink("uploads/"+req.params.filename, function (err) {
+      if (err) {
+        res.send("An error occurred while trying to delete the apk file from server");
+      } else {
+        res.send("Energy evaluation finished");
+      }
+    });
   });
 });
 
