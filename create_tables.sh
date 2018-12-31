245f3fc1a38990a49c1c61c93f90de7415a42e89
Dennis Tsiang <dt1015@ic.ac.uk>
2018-12-31 10:54:37 +0000

    Add new create_tables.sh script
    This script helps to create the tables for all the categories. Adding
    new categories simply needs to be added to the array.

---
+++ create_tables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+array=("News" "Gaming" "Business" "Social_Media" "Lifestyle" "Productivity" "Photography" "Video_Players_Editors")
+for item in ${array[*]}
+do
+    echo "create table ${item} (id SERIAL PRIMARY KEY, total_hardware_energy DOUBLE PRECISION, total_routine_energy DOUBLE PRECISION, rating VARCHAR(40) NOT NULL, created_at TIMESTAMP DEFAULT NOW() );"
+done | psql boostergy
+
